881437e6662ef97135390c1efeca7a26f374110b
ARUNKUMARVASUDEVAN <oyeahitsarun@gmail.com>
2023-06-28 19:51:42 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+Hyphen_e_dot='-e .'
+
+def get_requirements(file_path):
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if Hyphen_e_dot in requirements:
+            requirements.remove(Hyphen_e_dot)
+
+    return requirements
+
+
+setup(
+    name="Flight_Fare_Prediction",
+    version="0.0.1",
+    author="Arunkumar",
+    author_email='oyeahitsarun@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+
+)
+
+
